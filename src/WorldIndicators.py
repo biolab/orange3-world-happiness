99f106d1bf5bb262f3160c510c4f11a10b8ddf1b
Toxic <nhirci@gmail.com>
2021-10-15 13:11:07 +0200

    Finished update for WDB/WDI.

--- src/WorldIndicators.py
+++ src/WorldIndicators.py
@@ -5,13 +5,32 @@
 # Author: Nejc Hirci
 # -----------------------------------------------------------
 
+from pprint import pprint
 import wbgapi as wb
 from pymongo import MongoClient
+from pymongo import UpdateOne
 
 
 MONGODB_HOST = 'localhost'
 MONGODB_PORT = 27017
-DB_NAME = 'world_happiness_indicators'
+DB_NAME = 'world-indicators'
+
+
+def find_country_name(code):
+    economy = list(wb.economy.list(code))
+    if len(economy) == 0:
+        raise ValueError(f"Invalid country code {code}.")
+
+    return economy[0]['value']
+
+
+def find_indicator_desc(code):
+    indicator = list(wb.series.list(code))
+    if len(indicator) == 0:
+        raise ValueError(f"Invalid indicator code {code}.")
+
+    return indicator[0]['value']
+
 
 class WorldIndicators:
 
@@ -24,7 +43,9 @@ class WorldIndicators:
         """ Set up connection to local mongoDB database
         :return: database object
         """
-        client = MongoClient("mongodb://{}:{}@{}:{}/".format(self.user, self.pwd, MONGODB_HOST, MONGODB_PORT))
+        uri = f"mongodb://{self.user}:{self.pwd}@{MONGODB_HOST}:{MONGODB_PORT}/?authSource={DB_NAME}&authMechanism" \
+              f"=SCRAM-SHA-256"
+        client = MongoClient(uri)
         return client[DB_NAME]
 
     def countries(self):
@@ -33,13 +54,11 @@ class WorldIndicators:
         """
         return list()
 
-
     def years(self):
         """ Function gets data from local database.
         :return: list of years with data
         """
         return list()
-
 
     def indicators(self):
         """ Function gets data from local database.
@@ -48,8 +67,7 @@ class WorldIndicators:
         """
         return list()
 
-
-    def data(self, countries, indicators, year):
+    def data(self, countries, indicators, years):
         """ Function gets data from local database.
         :param countries: list of country codes
         :type countries: list
@@ -61,40 +79,86 @@ class WorldIndicators:
         """
         return list()
 
-
-    def data(self, countries, indicators, year):
-        """ Function gets data from local database.
-        :param countries:
-        :param indicators:
-        :param year: list of years
-        :type year: list
-        :return: list of indicators
-        """
-
-
-    def update(self, countries, indicators, year, db):
+    def update(self, countries, indicators, years, db):
         """ Refreshes the local database from a given db database.
         :param countries: list of country codes
         :type countries: list
         :param indicators: list of indicator codes
         :type indicators: list
-        :param year: year for data
-        :type year: int
+        :param years: list of years
+        :type years: list
         :param db: database
         :type db: str
         :return: list of indicators
         """
 
-        if db == "WDI":
+        # First get country documents if they don't exist create them
+        documents = {}
+        collection = self.db.countries
+        for code in countries:
+            name = find_country_name(code)
+            if len(list(collection.find({"_id": code}).limit(1))) == 0:
+                doc = {
+                    "_id": code,
+                    "name": name,
+                    "indicators": {}
+                }
+            else:
+                doc = collection.find_one({"_id": code})
+            documents.update({code: doc})
 
-        elif db == "WHR":
+        # Second create indicator documents if they don't exist
+        for code in indicators:
+            if len(list(self.db.indicators.find({"_id": code}).limit(1))) == 0:
+                doc = {
+                    "_id": code,
+                    "desc": find_indicator_desc(code),
+                    "db": db,
+                    "url": None
+                }
+                self.db.indicators.insert_one(doc)
+
+        if db == 'WDI' or db == 'WBD':
+            # Fetch data from WBD
+            wb.db = 2
+            data = wb.data.DataFrame(indicators, economy=countries, time=years)
+            data.reset_index(inplace=True)
+            data_dict = data.to_dict("records")
+
+            # Select mongo collection of countries
+            collection = self.db.countries
+
+            # Get data and update local python object
+            for row in data_dict:
+                doc = documents[row['economy']]
+                print(f"Update document for country {row['economy']} for {row['series']}")
+
+                if hasattr(doc['indicators'], row['series']):
+                    indicator = doc['indicators'][row['series']]
+                else:
+                    doc['indicators'][row['series']] = {}
+                    indicator = doc['indicators'][row['series']]
 
 
-        # TODO: Optimization depending on database interface using multiple calls
+                for key, val in row.items():
+                    if 'YR' in key:
+                        indicator.update({key[2:]: val})
 
-        return list()
+            # Update documents in local mongo database
+            operations = []
+            for _, doc in documents.items():
+                operations.append(UpdateOne({"_id": doc['_id']}, {"$set": doc}, upsert=True))
+
+            result = collection.bulk_write(operations)
+            pprint(result.bulk_api_result)
+
+        elif db == 'WHR':
+            # TODO Implement WHR update function
+            pass
 
 
 if __name__ == "__main__":
-    data = WorldIndicators("admin", "biolab")
-    print(data.db)
+    data = WorldIndicators("main", "biolab")
+
+    # Test querry
+    data.update(['USA', 'CAN'], ['NY.GDP.PCAP.CD', 'SP.POP.TOTL'], list(range(1920, 2020)), 'WDI')

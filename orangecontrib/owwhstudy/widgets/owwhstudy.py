87a7d2c9d159a3d5fec9199d3809ce24ff76bd26
Nejc Hirci <nhirci@gmail.com>
2022-01-05 17:25:40 +0100

    Working on drag n drop indicator view #4

--- orangecontrib/owwhstudy/widgets/owwhstudy.py
+++ orangecontrib/owwhstudy/widgets/owwhstudy.py
@@ -1,6 +1,7 @@
 from typing import Any, List, Set
+from functools import partial
 
-from AnyQt.QtCore import Qt, Signal, QSortFilterProxyModel, QItemSelection, QItemSelectionModel
+from AnyQt.QtCore import Qt, Signal, QSortFilterProxyModel, QItemSelection, QItemSelectionModel, QTimer
 from AnyQt.QtWidgets import QLabel, QGridLayout, QFormLayout, QLineEdit, \
     QTableView, QListView, QTreeWidget, QTreeWidgetItem
 from AnyQt.QtGui import QStandardItem
@@ -8,14 +9,29 @@ from AnyQt.QtGui import QStandardItem
 from Orange.data import Table
 from Orange.data.pandas_compat import table_from_frame
 from Orange.widgets.settings import Setting, ContextSetting
+from Orange.widgets.utils import vartype
 from Orange.widgets.utils.concurrent import ConcurrentWidgetMixin, TaskState
 from Orange.widgets.utils.itemmodels import PyTableModel, TableModel
+from Orange.widgets.utils.listfilter import (
+    VariablesListItemView, slices, variables_filter
+)
 from Orange.widgets.widget import OWWidget, Output
+from Orange.widgets.data.owselectcolumns import VariablesListItemModel
 from Orange.widgets import gui
 
 from orangecontrib.owwhstudy.whstudy import WorldIndicators, AggregationMethods
 
 MONGO_HANDLE = WorldIndicators('main', 'biolab')
+
+
+def source_model(view):
+    """ Return the source model for the Qt Item View if it uses
+    the QSortFilterProxyModel.
+    """
+    if isinstance(view.model(), QSortFilterProxyModel):
+        return view.model().sourceModel()
+    else:
+        return view.model()
 
 
 def run(
@@ -59,7 +75,7 @@ class CountryTreeWidgetItem(QTreeWidgetItem):
         self.country_code = code
 
 
-class IndexTableView(QTableView):
+class IndicatorTableView(QTableView):
     pressedAny = Signal()
 
     def __init__(self):
@@ -119,23 +135,35 @@ class OWWHStudy(OWWidget, ConcurrentWidgetMixin):
         OWWidget.__init__(self)
         ConcurrentWidgetMixin.__init__(self)
         super().__init__()
+
+        # Schedule interface updates (enabled buttons) using a coalescing
+        # single shot timer (complex interactions on selection and filtering
+        # updates in the 'available_attrs_view')
+        self.__interface_update_timer = QTimer(self, interval=0, singleShot=True)
+        self.__interface_update_timer.timeout.connect(
+            self.__update_interface_state)
+        # The last view that has the selection for move operation's source
+        self.__last_active_view = None  # type: Optional[QListView]
+
         self.world_data = None
         self.year_features = []
         self.country_features = MONGO_HANDLE.countries()
         self.indicator_features = MONGO_HANDLE.indicators()
-        self.indicator_model = IndicatorTableModel(parent=self)
+
         self._setup_gui()
 
         # Assign values to control views
         self.year_features = [str(y) for y in range(2020, 1960, -1)]
-        self.indicator_model.wrap(self.indicator_features)
-        self.indicator_model.setHorizontalHeaderLabels(['Source', 'Index', 'Relative', 'Description'])
-        self.indicator_view.resizeColumnToContents(0)
-        self.indicator_view.resizeColumnToContents(1)
-        self.indicator_view.resizeRowsToContents()
+
         ctree = self.create_country_tree(self.country_features)
         self.country_tree.itemChanged.connect(self.country_checked)
         self.set_country_tree(ctree, self.country_tree)
+
+        self.available_indices_model.wrap(self.indicator_features)
+        self.available_indices_model.setHorizontalHeaderLabels(['Source', 'Index', 'Relative', 'Description'])
+        self.available_indices_view.resizeColumnToContents(0)
+        self.available_indices_view.resizeColumnToContents(1)
+        self.available_indices_view.resizeRowsToContents()
 
     def _setup_gui(self):
         fbox = gui.widgetBox(self.controlArea, "", orientation=0)
@@ -163,10 +191,6 @@ class OWWHStudy(OWWidget, ConcurrentWidgetMixin):
         gui.auto_send(bbox, self, "auto_apply")
 
         box = gui.widgetBox(fbox, "Countries")
-        self.__country_filter_line_edit = QLineEdit(
-            textChanged=self.__on_country_filter_changed,
-            placeholderText="Filter..."
-        )
 
         self.country_tree = QTreeWidget()
         self.country_tree.setFixedWidth(400)
@@ -175,39 +199,102 @@ class OWWHStudy(OWWidget, ConcurrentWidgetMixin):
         self.country_tree.setHeaderLabels(['Countries'])
         box.layout().addWidget(self.country_tree)
 
-        box = gui.widgetBox(self.mainArea, "Indicator Selection")
+        def update_on_change(view):
+            # Schedule interface state update on selection change in `view`
+            self.__last_active_view = view
+            self.__interface_update_timer.start()
+
+        box = gui.widgetBox(self.mainArea, "Available Indicators")
         self.__indicator_filter_line_edit = QLineEdit(
             textChanged=self.__on_indicator_filter_changed,
-            placeholderText="Filter..."
+            placeholderText="Filter ..."
         )
         box.layout().addWidget(self.__indicator_filter_line_edit)
 
-        self.indicator_view = IndexTableView()
-        self.indicator_view.horizontalHeader().sectionClicked.connect(
-            self.__on_indicator_horizontal_header_clicked)
-        box.layout().addWidget(self.indicator_view)
-
+        self.available_indices_model = IndicatorTableModel()
+        self.available_indices_view = IndicatorTableView()
         proxy = IndicatorFilterProxyModel()
         proxy.setFilterKeyColumn(-1)
         proxy.setFilterCaseSensitivity(Qt.CaseSensitivity.CaseInsensitive)
-        self.indicator_view.setModel(proxy)
-        self.indicator_view.setSortingEnabled(True)
-        self.indicator_view.model().setSourceModel(self.indicator_model)
-        self.indicator_view.selectionModel().selectionChanged.connect(
-            self.__on_indicator_selection_changed
-        )
+        self.available_indices_view.setModel(proxy)
+        self.available_indices_view.setSortingEnabled(True)
+        self.available_indices_view.model().setSourceModel(self.available_indices_model)
 
-    def __on_country_filter_changed(self):
-        pass
+        def dropcompleted(action):
+            if action == Qt.MoveAction:
+                self.commit.deferred()
+
+        self.available_indices_view.selectionModel().selectionChanged.connect(
+            partial(update_on_change, self.available_indices_view)
+        )
+        self.available_indices_view.dragDropActionDidComplete.connect(dropcompleted)
+        box.layout().addWidget(self.available_indices_view)
+
+        # ∨
+
+        box = gui.widgetBox(self.mainArea, margin=0)
+        self.move_indicators_button = gui.button(
+            box, self, "∧",
+            callback=partial(self.move_selected, self.used_indices_view)
+        )
+        box.layout().addWidget(self.move_indicators_button)
+
+        box = gui.widgetBox(self.mainArea, "Selected Indicators")
+        self.selected_indices_model = IndicatorTableModel()
+        self.selected_indices_view = IndicatorTableView()
+        self.selected_indices_view.model().setSourceModel(self.selected_indicators_model)
+        self.selected_indices_model.rowsInserted.connect(self.__selected_indicators_changed)
+        self.selected_indices_model.rowsRemoved.connect(self.__selected_indicators_changed)
+        self.selected_indices_view.selectionModel().selectionChanged.connect(
+            partial(update_on_change, self.selected_indices_view)
+        )
+        self.selected_indices_view.dragDropActionDidComplete.connect(dropcompleted)
+        box.layout().addWidget(self.selected_indices_view)
+
+    def __update_interface_state(self):
+        last_view = self.__last_active_view
+        if last_view is not None:
+            self.update_interface_state(last_view)
+
+    def update_interface_state(self, focus=None):
+        for view in [self.available_indices_view, self.selected_indicators_view]:
+            if view is not focus and not view.hasFocus() \
+                    and view.selectionModel().hasSelection():
+                view.selectionModel().clear()
+
+        def selected_indices(view):
+            model = source_model(view)
+            return [model[i] for i in self.selected_rows(view)]
+
+        available_selected = selected_indices(self.available_indices_view)
+        indices_selected = selected_indices(self.selected_indices_view)
+
+        move_indices_enabled = \
+            (available_selected or indices_selected) and \
+            self.selected_indices_view.isEnabled()
+        self.move_indicators_button.setEnabled(bool(move_indices_enabled))
+        if move_indices_enabled:
+            self.move_indicators_button.setText("∨" if available_selected else "∧")
+
+        self.__last_active_view = None
+        self.__interface_update_timer.stop()
 
     def __on_indicator_filter_changed(self):
         model = self.indicator_view.model()
         model.setFilterFixedString(self.__indicator_filter_line_edit.text().strip())
         self._select_indicator_rows()
 
+    def __selected_indicators_changed(self):
+        indicators = self.selected_indicators
+        available, selected = self.available_indices_model[:], self.selected_indices_model[:]
+        self.available_indices_model[:] = [attr for attr in selected + available
+                                           if attr not in indicators]
+        self.selected_indices_model[:] = indicators
+        self.commit()
+
     def __on_indicator_selection_changed(self):
-        selected_rows = self.indicator_view.selectionModel().selectedRows(1)
-        model = self.indicator_view.model()
+        selected_rows = self.available_indices_view.selectionModel().selectedRows(1)
+        model = self.available_indices_view.model()
         self.selected_indicators = [
             (model.data(model.index(i.row(), 0)),
              model.data(model.index(i.row(), 1)),
@@ -313,6 +400,70 @@ class OWWHStudy(OWWidget, ConcurrentWidgetMixin):
         self.indicator_view.selectionModel().select(
             selection, QItemSelectionModel.ClearAndSelect
         )
+
+    ### DRAG AND DROP FUNCTIONS
+
+    @staticmethod
+    def selected_rows(view):
+        """ Return the selected rows in the view.
+        """
+        rows = view.selectionModel().selectedRows()
+        model = view.model()
+        if isinstance(model, QSortFilterProxyModel):
+            rows = [model.mapToSource(r) for r in rows]
+        return [r.row() for r in rows]
+
+    def move_rows(self, view: QListView, offset: int, roles=(Qt.EditRole,)):
+        rows = [idx.row() for idx in view.selectionModel().selectedRows()]
+        model = view.model()  # type: QAbstractItemModel
+        rowcount = model.rowCount()
+        newrows = [min(max(0, row + offset), rowcount - 1) for row in rows]
+
+        def itemData(index):
+            return {role: model.data(index, role) for role in roles}
+
+        for row, newrow in sorted(zip(rows, newrows), reverse=offset > 0):
+            d1 = itemData(model.index(row, 0))
+            d2 = itemData(model.index(newrow, 0))
+            model.setItemData(model.index(row, 0), d2)
+            model.setItemData(model.index(newrow, 0), d1)
+
+        selection = QItemSelection()
+        for nrow in newrows:
+            index = model.index(nrow, 0)
+            selection.select(index, index)
+        view.selectionModel().select(
+            selection, QItemSelectionModel.ClearAndSelect)
+
+        self.commit
+
+    def move_up(self, view: QListView):
+        self.move_rows(view, -1)
+
+    def move_down(self, view: QListView):
+        self.move_rows(view, 1)
+
+    def move_selected(self, view):
+        if self.selected_rows(view):
+            self.move_selected_from_to(view, self.available_indices_view)
+        elif self.selected_rows(self.available_indices_view):
+            self.move_selected_from_to(self.available_indices_view, view)
+
+    def move_selected_from_to(self, src, dst):
+        self.move_from_to(src, dst, self.selected_rows(src))
+
+    def move_from_to(self, src, dst, rows):
+        src_model = source_model(src)
+        indices = [src_model[r] for r in rows]
+
+        for s1, s2 in reversed(list(slices(rows))):
+            del src_model[s1:s2]
+
+        dst_model = source_model(dst)
+
+        dst_model.extend(indices)
+
+        self.commit()
 
 
 if __name__ == "__main__":

36d6b8c2a5b34d1eaaf3351937642c5dbbfb7b21
Toxic <nhirci@gmail.com>
2022-01-26 11:35:25 +0100

    Updated indicator descriptions in database #15

---
+++ indicator_update.py
@@ -0,0 +1,68 @@
+from whstudy.world_data_api import WorldIndicators
+import pandas as pd
+import numpy as np
+
+def get_code_names(indic_code, df):
+    codes = indic_code.split('_')
+    code_names = []
+    topic = codes[0]
+    general_subject = codes[1]
+    specific_subject = codes[2]
+    extensions = codes[3:]
+
+    s = df.loc[df['Topic'] == topic, 'Topic description']
+    if s.size > 0:
+        code_names.append(s.iat[0])
+    s = df.loc[df['General subject'] == general_subject, 'General subject description']
+    if s.size > 0:
+        code_names.append(s.iat[0])
+    s = df.loc[df['Specific subject'] == specific_subject, 'Specific subject description']
+    if s.size > 0:
+        code_names.append(s.iat[0])
+
+    for extension in extensions:
+        s = df.loc[df['Extensions'] == extension, 'Extensions description']
+        if s.size > 0:
+            code_names.append(s.iat[0])
+
+    return code_names
+
+if __name__ == "__main__":
+    handle = WorldIndicators("main", "biolab")
+    indicators = handle.indicators()
+
+    # df = pd.read_csv('./data/wdi/wdi_cets.csv')
+    # df = df.set_index('Series Code')
+    df = pd.read_csv('./data/wdi/wdi_short_codes.csv')
+
+    for i in indicators:
+        if i[0] == 'WDI':
+            indic_code = str.replace(i[1], '.', '_')
+            doc = handle.db.indicators.find_one({"_id": indic_code})
+            print(indic_code)
+            code_names = get_code_names(indic_code, df)
+            new_doc = {
+                "_id": doc["_id"],
+                "db": doc["db"],
+                "code_exp": code_names,
+                "desc": doc["desc"],
+                "is_relative": doc["is_relative"],
+                "url": doc["url"]
+            }
+            handle.db.indicators.replace_one({"_id": indic_code}, new_doc)
+
+
+    # for (db, code, is_relative, desc) in indicators:
+    #     indic_code = str.replace(code, ".", "_")
+    #     doc = handle.db.indicators.find_one({"_id": indic_code})
+    #     if code in df.index:
+    #         if not pd.isnull(df.at[code, 'Topic']):
+    #             doc['Topic'] = df.at[code, 'Topic']
+    #         if not pd.isnull(df.at[code, 'SubTopic1']):
+    #             doc['SubTopic1'] = df.at[code, 'SubTopic1']
+    #         if not pd.isnull(df.at[code, 'SubTopic2']):
+    #             doc['SubTopic2'] = df.at[code, 'SubTopic2']
+    #         if not pd.isnull(df.at[code, 'SubTopic3']):
+    #             doc['SubTopic3'] = df.at[code, 'SubTopic3']
+    #         handle.db.indicators.replace_one({"_id": indic_code}, doc)
+

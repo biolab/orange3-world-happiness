5e7c41f68cb24fc34b6dbd27260e28b50377b02d
Toxic <nhirci@gmail.com>
2022-08-26 16:09:32 +0200

    Release 0.1.8

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with io.open('README.pypi', 'r', encoding='utf-8') as f:
 
 NAME = "Orange3-WorldHappiness"
 
-VERSION = "0.1.7"
+VERSION = "0.1.8"
 
 AUTHOR = 'Bioinformatics Laboratory, FRI UL'
 AUTHOR_EMAIL = 'contact@orange.biolab.si'

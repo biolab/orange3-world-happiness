c706bda76c8660ba2f70c739cff5984c7125ce3f
PrimozGodec <p.godec9@gmail.com>
2024-03-20 09:26:53 +0100

    Fix documentation path

--- setup.py
+++ setup.py
@@ -65,21 +65,20 @@ NAMESPACE_PACKAGES = ["orangecontrib"]
 
 def include_documentation(local_dir, install_dir):
     global DATA_FILES
-    if 'bdist_wheel' in sys.argv and not path.exists(local_dir):
-        print("Directory '{}' does not exist. "
-              "Please build documentation before running bdist_wheel."
-              .format(path.abspath(local_dir)))
-        sys.exit(0)
 
     doc_files = []
-    for dirpath, dirs, files in walk(local_dir):
-        doc_files.append((dirpath.replace(local_dir, install_dir),
-                          [path.join(dirpath, f) for f in files]))
+    for dirpath, _, files in walk(local_dir):
+        doc_files.append(
+            (
+                dirpath.replace(local_dir, install_dir),
+                [path.join(dirpath, f) for f in files],
+            )
+        )
     DATA_FILES.extend(doc_files)
 
 
 if __name__ == '__main__':
-    include_documentation('doc/_build/htmlhelp', 'help/orange3-worldhappiness')
+    include_documentation('doc/_build/html', 'help/orange3-worldhappiness')
     setup(
         name=NAME,
         version=VERSION,
